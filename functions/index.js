5b4a7df32ca56aa4ac155579e2bf7870cf579c6d
Adrienne Dreyfus <adreyfus@stripe.com>
2019-11-13 12:38:24 -0800

    Updates

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const functions = require("firebase-functions");
 
-exports.handleDBWrite = functions.handler.database.ref.onCreate(snap => {
+exports.handleDBWrite = functions.database.ref("/invoices").onCreate(snap => {
   console.log("Detected change!");
   return "";
 });
