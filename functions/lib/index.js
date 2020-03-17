28047a15947ebab3426e4a68709e5a8cbb5a0f15
jhuleatt <jhuleatt@gmail.com>
2020-03-17 11:05:15 -0700

    various build/config files

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -13,7 +13,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const stripe_1 = __importDefault(require("stripe"));
 const functions = __importStar(require("firebase-functions"));
 const logs = __importStar(require("./logs"));
-const admin = require("firebase-admin");
+const admin = __importStar(require("firebase-admin"));
 const stripe = new stripe_1.default(process.env.STRIPE_SECRET_KEY, {
     apiVersion: "2019-12-03"
 });
@@ -85,11 +85,16 @@ exports.sendInvoice = functions.handler.firestore.document.onCreate(async (snap)
         }
         const invoice = await createInvoice(customer, payload.items, daysUntilDue);
         if (invoice.id) {
+            // Write the Stripe Invoice ID back to the document in Firestore
+            // so that we can find it in the webhook
+            await snap.ref.update({
+                stripeInvoiceId: invoice.id
+            });
             // Email the invoice to the customer
             const result = await stripe.invoices.sendInvoice(invoice.id);
             if (result.status === "open") {
                 // Successfully emailed the invoice
-                logs.invoiceSent(result.id, payload.email, result.hosted_invoice_url);
+                logs.invoiceSent(result.id, email, result.hosted_invoice_url);
             }
             else {
                 logs.invoiceCreatedError(result);
@@ -103,5 +108,81 @@ exports.sendInvoice = functions.handler.firestore.document.onCreate(async (snap)
         logs.error(e);
     }
     return;
+});
+// call this function to easily
+exports.dummyData = functions.handler.https.onRequest(async (req, resp) => {
+    const db = admin.firestore();
+    db.collection(process.env.DB_PATH).add({
+        email: "test@tester.com",
+        items: [
+            {
+                amount: 2000,
+                currency: "usd",
+                description: "Growth plan"
+            }
+        ]
+    });
+});
+const relevantInvoiceEvents = new Set([
+    "invoice.created",
+    "invoice.finalized",
+    "invoice.payment_failed",
+    "invoice.payment_succeeded"
+]);
+exports.updateInvoice = functions.handler.https.onRequest(async (req, resp) => {
+    let invoice;
+    let event;
+    try {
+        invoice = req.body.data.object;
+        event = req.body.type;
+    }
+    catch (err) {
+        resp.status(400).send(`Webhook Error: ${err.message}`);
+    }
+    if (!relevantInvoiceEvents.has(event)) {
+        console.log(`Ignoring event "${event}" because it isn't a relevant part of the invoice lifecycle`);
+        // Return a response to acknowledge receipt of the event
+        resp.json({ received: true });
+        return;
+    }
+    let invoicesInFirestore = await admin
+        .firestore()
+        .collection(process.env.DB_PATH)
+        .where("stripeInvoiceId", "==", invoice.id)
+        .get();
+    // If we don't have exactly 1 invoice, something went wrong
+    if (invoicesInFirestore.size === 0) {
+        // throw new Error(`Could not find invoiceId "${invoice.id}"`);
+        /// TEMPORARY
+        await admin
+            .firestore()
+            .collection(process.env.DB_PATH)
+            .add({
+            stripeInvoiceId: invoice.id,
+            email: "test@tester.com",
+            items: [
+                {
+                    amount: 2000,
+                    currency: "usd",
+                    description: "Growth plan"
+                }
+            ]
+        });
+        invoicesInFirestore = await admin
+            .firestore()
+            .collection(process.env.DB_PATH)
+            .where("stripeInvoiceId", "==", invoice.id)
+            .get();
+        ///
+    }
+    else if (invoicesInFirestore.size > 1) {
+        throw new Error(`Found multiple items matching invoiceId "${invoice.id}"`);
+    }
+    const doc = invoicesInFirestore.docs[0];
+    await doc.ref.update({
+        stripeInvoiceStatus: event
+    });
+    console.log(`Updated invoice "${invoice.id}" to status "${event}"`);
+    resp.json({ received: true });
 });
 //# sourceMappingURL=index.js.map
