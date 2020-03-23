1e4a0c34d8c130fe7d0e4b1268ecacc9a4dae6dc
jhuleatt <jhuleatt@gmail.com>
2020-03-23 09:27:00 -0700

    add more invoice events

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -131,7 +131,10 @@ const relevantInvoiceEvents = new Set([
   "invoice.created",
   "invoice.finalized",
   "invoice.payment_failed",
-  "invoice.payment_succeeded"
+  "invoice.payment_succeeded",
+  "invoice.payment_action_required",
+  "invoice.voided",
+  "invoice.marked_uncollectible"
 ]);
 
 export const updateInvoice = functions.handler.https.onRequest(
